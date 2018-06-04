38c1a777ad9d5db86a54f8a6e8af9fe0ef0263e4
Hideto Yasunori <hideto0118yasunori@gmail.com>
2018-06-04 13:26:21 -0700

    Turnt the triangle into gradient

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var vertexShaderText =
 '',
 'attribute vec2 vertPosition;',
 'attribute vec3 vertColor;',
-'varying vac3 fragColor;'
+'varying vec3 fragColor;',
 '',
 'void main()',
 '{',
-' fragColor = vertColor;'
+' fragColor = vertColor;',
 ' gl_Position = vec4(vertPosition, 0.0, 1.0);',
 '}'
 ].join('\n');
@@ -86,7 +86,7 @@ var initDemo = function() {
   [// X, Y         R, G, B
     0.0, 0.5,      1.0, 1.0, 0.0,
     -0.5, -0.5,    0.7, 0.0, 1.0,
-    0.5, -0.5      0.1, 1.0, 0.6
+    0.5, -0.5,     0.1, 1.0, 0.6
   ];
 
   var triangleVertexBufferObject = gl.createBuffer();
@@ -94,16 +94,26 @@ var initDemo = function() {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticies), gl.STATIC_DRAW)
 
   var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+  var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
   gl.vertexAttribPointer(
     positionAttribLocation, // Attribute Location
     2, // Number of elements per sttribute
     gl.FLOAT, // Type of elements
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+    5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
     0 // Offset from the beginning of a single vertex to this attribute
+  );
+  gl.vertexAttribPointer(
+    colorAttribLocation, // Attribute Location
+    3, // Number of elements per sttribute
+    gl.FLOAT, // Type of elements
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+    2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
   );
 
   gl.enableVertexAttribArray(positionAttribLocation);
+  gl.enableVertexAttribArray(colorAttribLocation);
 
   //
   // Main render loop
