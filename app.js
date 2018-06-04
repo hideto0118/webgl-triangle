52e0f369adc821c499e08e71018e1a508bb92688
Hideto Yasunori <hideto0118yasunori@gmail.com>
2018-06-04 13:34:04 -0700

    Add style sheet

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var initDemo = function() {
   }
 
   // gl.clearColor(R, G, B, alpha);
-  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clearColor(0, 0, 0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
